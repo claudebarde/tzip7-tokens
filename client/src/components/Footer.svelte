48d886152b506fac139d35df75e44d6e8350aea5
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-05-25 15:24:59 +0300

    Added icons to footer on Landing page

--- client/src/components/Footer.svelte
+++ client/src/components/Footer.svelte
@@ -1,20 +1,44 @@
 <style>
   footer {
     padding: 4px;
+    display: flex;
+    justify-content: space-between;
   }
 
-  .github-logo {
-    width: 16px;
+  .links {
+    padding: 5px;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  .logo {
+    width: 22px;
     vertical-align: top;
   }
 </style>
 
-<footer class="has-text-centered is-size-7">
-  :: Claude Barde 2020
-  <a
-    href="https://github.com/claudebarde/tzip7-tokens"
-    rel="noopener noreferrer">
-    <img class="github-logo" src="images/GitHub-Mark-32px.png" alt="github" />
-  </a>
-  ::
+<footer class="is-size-7">
+  <div />
+  <div class="text">
+    <span>:: Claude Barde 2020 ::</span>
+  </div>
+  <div class="links">
+    <a
+      href="https://github.com/claudebarde/tzip7-tokens"
+      target="_blank"
+      rel="noopener noreferrer">
+      <img class="logo" src="images/GitHub-Mark-32px.png" alt="github" />
+    </a>
+    &nbsp;
+    <a
+      href="https://gitlab.com/tzip/tzip/-/blob/master/proposals/tzip-7/tzip-7.md"
+      target="_blank"
+      rel="noopener noreferrer">
+      <img class="logo" src="images/gitlab-icon-rgb.png" alt="gitlab" />
+    </a>
+  </div>
 </footer>
