dfc74c34867a158e6333ed112f02f235c935f908
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-05-27 17:10:20 +0300

    Added link to AddToken contract on main page

--- client/src/components/Footer.svelte
+++ client/src/components/Footer.svelte
@@ -1,3 +1,7 @@
+<script>
+  import store from "../store";
+</script>
+
 <style>
   footer {
     padding: 4px;
@@ -27,6 +31,24 @@
     <span>:: Claude Barde 2020 ::</span>
   </div>
   <div class="links">
+    <a
+      href={`https://you.better-call.dev/carthagenet/${$store.contractAddress}/operations`}
+      target="_blank"
+      rel="noopener noreferrer">
+      <svg
+        class="logo"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        stroke="#333"
+        viewBox="0 0 24 24">
+        <path
+          d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1
+          0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+      </svg>
+    </a>
+    &nbsp;
     <a
       href="https://github.com/claudebarde/tzip7-tokens"
       target="_blank"
